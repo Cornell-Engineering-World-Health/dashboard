1bd19ab1cd2156de2c7c6ef0a2ff5f6e96c4bd31
Frank Chan <fc249@cornell.edu>
2015-12-06 13:05:25 -0500

    importing through json

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,7 @@
 //importing our Reading model, which has a predefined schema you can see in Readings.js
 var mongoose       = require('mongoose');
 var Reading        = require('./models/Readings.js');
+var data           = require('./data.json');
 
 module.exports = function(app) {
 
@@ -9,27 +10,8 @@ module.exports = function(app) {
 	// authentication routes	
 	// sample api route
 
-//dynamically generates the fake data
-//fake data is loaded upon GET request
-//you can see this data in http://localhost:8080/api/data
-var new_load = new Array();
-
-function push_new() {
-  var today = new Date();
-  var date = "" + (today.getMonth() + 1) + "/" + today.getDate() + "/" + today.getFullYear();
-
-  new_load.push({
-    temperature: Math.floor((Math.random() * 100) + 1),
-    turbidity: Math.floor((Math.random() * 7) + 1),
-    conductivity: Math.floor((Math.random() * 10) + 1),
-    pH: Math.floor((Math.random() * 6) + 4),
-    timestamp: date 
-  });
-}
-setInterval(push_new, 1000);
-
 app.get('/api/data', function(req, res) {
-  
+  console.log(data.length);
   for(i = 0; i < new_load.length; i++) {
     Reading.create({
       temperature: new_load[i].temperature,
