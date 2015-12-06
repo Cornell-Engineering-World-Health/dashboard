979cdacfe99375d31f2d97111511274eb2a276b8
Annie Tran <at697@cornell.edu>
2015-12-06 13:16:36 -0500

    background color added

---
+++ public/js/background.js
@@ -0,0 +1,101 @@
+
+
+	var badtemp = 90;
+	var okaytemp = 70;
+	var goodtemp = 50;
+	var badturb = 7;
+	var okayturb = 5;
+	var goodturb = 1;
+	var badcond = 10;
+	var okaycond = 7.5;
+	var goodcond = 5.8;
+	var badpH = 9.5;
+	var okaypH = 8.0;
+	var goodpH = 7.0;
+
+	// scaleTemp will scale the temperature to a number between 0 to 100 based on water quality quotas
+	function scaleTemp (temp){
+		if (temp > badtemp) {
+			return 100;
+		}
+		else if (temp <= badtemp && temp >= okaytemp){
+			return ((100 - 50) * (temp - okaytemp) / (badtemp - okaytemp)) + 50;
+		}
+		else if (temp < okaytemp && temp >= goodtemp){
+			return ((50 - 0) * (temp - goodtemp) / (okaytemp - goodtemp));
+		}
+		else {
+			return 0;
+		}
+	}
+	// scaleTurb will scale the turbidity to a number between 0 to 100 based on water quality quotas
+	function scaleTurb (turb){
+		if (turb > badturb) {
+			return 100;
+		}
+		else if (turb <= badturb && turb >= okayturb){
+			return ((100 - 50) * (turb - okayturb) / (badturb - okayturb)) + 50;
+		}
+		else if (turb < okayturb && turb >= goodturb) {
+			return ((50 - 0) * (turb - goodturb) / (okayturb - goodturb));
+		}
+		else {
+			return 0;
+		}
+	}
+	// scaleCond will scale the conductivity to a number between 0 to 100 based on water quality quotas
+	function scaleCond (cond){
+		if (cond > badcond) {
+			return 100;
+		}
+		else if (cond <= badcond && cond >= okaycond){
+			return ((100 - 50) * (cond - okaycond) / (badcond - okaycond)) + 50;
+		}
+		else if (cond < okaycond && cond >= goodcond) {
+			return ((50 - 0) * (cond - goodcond) / (okaycond - goodcond));
+		}
+		else 
+		{
+			return 0;
+		}
+	}
+	// scalepH will scale the pH to a number between 0 to 100 based on water quality quotas
+	function scalepH (pH){
+		if (pH > badpH) {
+			return 100;
+		}
+		else if (pH <= badpH && pH >= okaypH){
+			return ((100 - 50) * (pH - okaypH) / (badpH - okaypH)) + 50;
+		}
+		else if (pH < okaypH && pH >= goodpH) {
+			return ((50 - 0) * (pH - goodpH) / (okaypH - goodpH));
+		}
+		else {
+			return 0;
+		}
+	}
+
+	//waterQuality will calculate an average of all the scaled values of temperature, turbidity, conductivity, and pH
+	function waterQuality (temp, turb, cond, pH){
+		return ((scaleTemp (temp) + scaleTurb (turb) + 
+			scaleCond (cond) + scalepH (pH))/4);
+	}
+
+	function changeBackground(temp, turb, cond, pH) {
+		if (scaleTemp (temp) >= 100 || scaleTurb (turb) >= 100 || 
+			scaleCond (cond) >= 100 || scalepH (pH) >= 100){
+			document.body.style.background = "#e60000";
+		}
+		else if (scaleTemp (temp) >= 50  || scaleTurb (turb) >= 50 || 
+			scaleCond (cond) >= 50 || scalepH (pH) >= 50){
+			document.body.style.background = "#ffff66";
+		}
+		else{
+			document.body.style.background = "green";
+		}
+			
+	}
+
+		
+
+		
