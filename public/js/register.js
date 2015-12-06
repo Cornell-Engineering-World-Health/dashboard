0f34c65a0c307abc0149cff01fab04c5e9c9d640
Annie Tran <at697@cornell.edu>
2015-12-06 14:04:01 -0500

    register.js file added

---
+++ public/js/register.js
@@ -0,0 +1,39 @@
+var exports = module.exports = {
+
+	exports.bad_temp = function (){
+		return 90;
+	},
+	exports.okay_temp = function(){
+		return 70;
+	},
+	exports.good_temp = function(){
+		return 50;
+	},
+	exports.bad_turb = function(){
+		return 7;
+	},
+	exports.okay_turb = function(){
+		return 5;
+	},
+	exports.good_turb = function:(){
+		return 1;
+	},
+	exports.bad_cond = function(){
+		return 10;
+	},
+	exports.okay_cond = function(){
+		return 7.5;
+	},
+	exports.good_cond = function(){
+		return 5.8;
+	},
+	exports.bad_pH = function(){
+		return 9.5;
+	},
+	exports.okay_pH = function(){
+		return 8.0;
+	},
+	exports.good_pH = function(){
+		return 7.0;
+	} 
+};
